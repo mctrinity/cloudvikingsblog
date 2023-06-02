d48af4f541a5eabd18b73768e3bf4eaa8fd3d9f1
goodbyegirl <mctrinity24@gmail.com>
2023-06-02 12:46:36 +0800

    updated files

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+
+    public function delete(Post $post) {
+        if (auth()->user()->cannot('delete', $post)) {
+            return 'You cannot do that';
+        }
+        $post-->delete();
+
+        return redirect('/profile/' . auth()->user()->username)->with('success', 'Post successfully deleted!');
+
+    }
+    public function viewSinglePost(Post $post) {
+        $post['body'] = strip_tags(Str::markdown($post->body), '<p><ul><ol><li><strong><em><h3><br>'); // post markdown
+        return view('single-post', ['post' => $post]);
+    }
+
+    public function storeNewPost(Request $request) {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+
+        $newPost = Post::create($incomingFields);
+        
+        return redirect("/post/{$newPost->id}")->with('success', 'New post successfully created!');
+    }
+
+    public function showCreateForm() {
+        return view('create-post');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// user related routes
+Route::get('/', [UserController::class, "showCorrectHomePage"])->name('login');
+Route::post('/register', [UserController::class, 'register'])->middleware('auth');
+Route::post('/login', [UserController::class, 'login'])->middleware('guest');
+Route::post('/logout', [UserController::class, 'logout'])->middleware('mustBeLoggedIn');
+
+// Blog post related routes
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn');
+Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('mustBeLoggedIn');
+Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
+Route::delete('/post/{post}', [PostController::class, 'delete']);
+
+// Profile related routes
+Route::get('/profile/{user:username}', [UserController::class, 'profile']);
+
+
